797706eddb30f5f38540167b541f9f2e4ae8ec7f
Kalyan-71 <164600184+Kalyan-71@users.noreply.github.com>
2025-03-05 19:58:23 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,19 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 
+
+
 export default defineConfig({
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 1000, // Increase limit (1000KB = 1MB)
   },
-});
+
+  css: {
+    preprocessorOptions: {
+      scss: {
+        silenceDeprecations: ["legacy-js-api"],
+      },
+    },
+  },
+})
